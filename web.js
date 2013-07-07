467e6ade79b74baac82d0a1c513350a5a97e6e60
varunchauhan5 <k.varun@outlook.com>
2013-07-07 10:35:16 +0530

    read content from index.html

--- web.js
+++ web.js
@@ -10,7 +10,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
